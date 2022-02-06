19623170a731571ef990b147eeaa6f62f71bd526
Shamunesh Selvam <64699355+SHAMUNESH@users.noreply.github.com>
2022-02-06 22:54:18 +0530

    files uploaded | AWS Cognito Testing v1 (6-02)

---
+++ build/precache-manifest.525e49a8f00bef9ee90cb776792f4015.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ceaf5bf97983ce4a3471",
+    "url": "/static/css/main.3030b531.chunk.css"
+  },
+  {
+    "revision": "ceaf5bf97983ce4a3471",
+    "url": "/static/js/main.ceaf5bf9.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "dfe1e3b46e0620334927",
+    "url": "/static/css/2.eeab7418.chunk.css"
+  },
+  {
+    "revision": "dfe1e3b46e0620334927",
+    "url": "/static/js/2.dfe1e3b4.chunk.js"
+  },
+  {
+    "revision": "885bf9bade73959e91b91cf0ed255147",
+    "url": "/index.html"
+  }
+];
